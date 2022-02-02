5e55e724b6c960b62d15a3a291e43209da409235
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-02-02 16:28:55 -0500

    commit tree 2ece6d726ddf222676cdd2e53cf18051f5db38a8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    let win = record.find(element => element.result === "W")
+    console.log(win)
+    if(win){
+        return win.year
+    } else {
+        return undefined
+    }
+}
